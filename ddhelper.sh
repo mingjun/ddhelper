d8230ed4144352899c2c8f8787a9c475903cdedb
Mingjun Xu <xmj0236@gmail.com>
2013-05-15 15:43:10 +0800

    crawler by category 1.0

--- ddhelper.sh
+++ ddhelper.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 TARGET_FILE=./ddbook.list
-LOG_FILE=./ddcrawler.log
+LOG_FILE=./dderror.log
 SLEEP_TIME=0.25
 
 CATEGORY=01.54.24.00.00.00
+COUNT=200
 
-SITE_URL="http://category.dangdang.com/all/?category_path=01.54.24.00.00.00&filter=0%7C0%7C1%7C0&page_index"
-COUNT=1
+SITE_URL="http://category.dangdang.com/all/?category_path="$CATEGORY"&filter=0%7C0%7C1%7C0&page_index"
 
 TRY_FILE=/tmp/dd.try.html
 TEMP_FILE=/tmp/dd.tmp.html
-
+MATRIX_TEMP=/tmp/dd.tmp.matrix
 
 touch $TRY_FILE
 touch $TEMP_FILE
@@ -19,10 +19,9 @@ for i in `seq $COUNT`
 do
     sleep $SLEEP_TIME
 
-
     echo crawl page No.$i
     # download
-#    curl $SITE_URL"=$i" > $TRY_FILE
+    curl $SITE_URL"=$i" > $TRY_FILE
     
     #check page validation
     if [ $( cat $TRY_FILE | wc -w ) -eq 0 ] 
@@ -37,25 +36,26 @@ do
 	| hxclean 2> /dev/null 1> $TEMP_FILE
 
     # parse info
+    product_link=$( cat $TEMP_FILE | hxselect ".shoplist>ul>li .name a" )
+    # check blank(end)
+    if [ $( echo $product_link | wc -w) -eq 0 ]
+    then
+	echo $i " end"
+	break
+    fi
 
-    cat $TEMP_FILE | hxselect -s "\n\n\n" ".shoplist>ul>li" |awk 'BEGIN {RS="\n\n\n"} {gsub(/[ \t\n\r]+/,  " FIXME "); print}'|\
-    while read line
-    do
-	each_li=$( echo $line | hxclean )
-	book_link=$( echo $each_li | hxselect ".name a" )
+    cat $TEMP_FILE | hxselect -c ".shoplist>ul>li .price_n" | awk 'BEGIN {RS="&yen;"}; /.+/ {print $1}' > $MATRIX_TEMP
+    echo "" >> $MATRIX_TEMP
+    cat $TEMP_FILE | hxselect -c ".shoplist>ul>li .price_r" | awk 'BEGIN {RS="&yen;"}; /.+/ {print $1}' >> $MATRIX_TEMP
+    echo "" >> $MATRIX_TEMP    
+    echo $product_link | awk 'BEGIN {RS="</a>"};/product_id=/ {match($0, /product_id=[0-9]+/);str=substr($0, RSTART, RLENGTH); gsub("product_id=","", str); print str}' >> $MATRIX_TEMP
+    echo "" >> $MATRIX_TEMP
+    echo $product_link | awk '{gsub("[ ]*<[^>]+>[ ]*", "\t"); print}' | awk 'BEGIN {RS="[\t]+"}; /.+/ {gsub(/[ ]+/ , "-"); print $1}' >> $MATRIX_TEMP
 
-	if [ -z "$book_link" ]
-	then
-	    echo "!!!" $line
-	    continue
-	fi
+    # append to target file
+    cat $MATRIX_TEMP | awk -f ./matrixT.awk >> $TARGET_FILE
 
-	book_name=$( echo $book_link | awk '{gsub(/<[^>]+>/, "")} {print}' )
-	echo $book_name
-
-    done
-#    book_name=$( cat $TEMP_FILE | hxselect "[name=Title_pub] h1" | awk '{gsub("<[^>]+>", ""); print}' )
-#    price=$( cat $TEMP_FILE | hxselect "#d_price" | awk '{gsub("<[^>]+>", ""); sub("&yen;", ""); print}' )
-#    pprice=$( cat $TEMP_FILE | hxselect ".m_price" | awk '{gsub("<[^>]+>", ""); sub("&yen;", ""); print}' )
-#    printf "%s\t%s\t%s\t%s\n" $price $pprice $pid "$book_name" >> $TARGET_FILE
 done
+
+
+
---
+++ matrixT.awk
@@ -0,0 +1,20 @@
+# Matrix Transpose
+BEGIN {
+    RS="\n\n"
+    FS="\n"
+}
+
+{
+    for(i=1; i <= NF; i++) {
+	m[FNR,i]=$i
+    }
+}
+
+END {
+    for(i=1; i<=NF; i++) {
+	for(j=1; j <= FNR; j++) {
+	    printf "%s%s", m[j, i], OFS
+	}
+	print ""
+    }
+}
